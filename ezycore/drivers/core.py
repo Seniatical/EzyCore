183911a8a7a51ca754b233223aeb133e910b0c1d
Seniatical <isaja7710@gmail.com>
2023-04-05 21:52:44 +0100

    numerous fixes to fetch/fetch_one methods

--- ezycore/drivers/core.py
+++ ezycore/drivers/core.py
@@ -1,68 +1,91 @@
 from __future__ import annotations
 from abc import ABC, abstractmethod
 
-from typing import Any, Dict, Iterator, Optional, Union
+from typing import Any, Dict, Iterator, Optional, Tuple, Union
+from typing_extensions import TypeAlias
+from os import PathLike
 
 from ezycore.models import Model
-from ezycore.manager import Segment
 
 
 # Iterator class used to accomodate varying data structs such as SQLs tuples
-RESULT = Union[Dict[str, Any], Model, Iterator]
+RESULT = Union[Dict[str, Any], Model, Tuple[Any]]
+StrOrBytesPath: TypeAlias = Union[str, bytes, PathLike[str], PathLike[bytes]]  
 
 
 class Driver(ABC):
     """ Base class for defining custom drivers """
 
     @abstractmethod
-    def fetch(self, query: Any, location: str = None, limit_result: int = -1, model: Model = None) -> Iterator[RESULT]:
+    def fetch(self, location: str, condition: Any = None, limit_result: int = -1, 
+              model: Model = None, *, raw: Any = None, no_handle: bool = False, ignore_model: bool = False,
+              **kwds
+    ) -> Iterator[RESULT]:
         """ Fetches as many results as possible based of query given
 
         Parameters
         ----------
-        query: Any
-            Query for filtering data
         location: :class:`str`
-            Name were data is being stored,
-            may vary between drivers
+            Place to fetch data from, varies between drivers.
+        condition: Any
+            Filter data based on a condition, varies between drivers.
         limit_result: :class:`int`
-            Number of results to limit to, 
-            values < 0 returns everything found
+            Limit how many results are returned,
+            if < 0, no limit is set
         model: :class:`Model`
-            Model to convert results to,
-            if no model present returns raw result
+            Model to return data as, 
+            if no model binded to location, this model becomes the default
+        raw: Any
+            Raw query data instead of using an auto generated one,
+            allows flexibility for some drivers such SQL based drivers
+        no_handle: :class:`bool`
+            Whether to just return the raw fetched data
+        ignore_model: :class:`bool`
+            Whether to return data as dict instead of model
+        **kwds:
+            Additional kwargs may be provided by other drivers which may require any
         """
 
     @abstractmethod
-    def fetch_one(self, query: Any, location: str = None, model: Model = None) -> Optional[RESULT]:
+    def fetch_one(self, location: str, condition: Any = None, model: Model = None, 
+                  *, raw: Any = None, no_handle: bool = False, ignore_model: bool = False,
+                  **kwds
+    ) -> Optional[RESULT]:
         """ Fetches only one result which matches the query
 
         Parameters
         ----------
-        query: Any
-            Query for filtering data
         location: :class:`str`
-            Name were data is being stored,
-            may vary between drivers
+            Place to fetch data from, varies between drivers.
+        condition: Any
+            Filter data based on a condition, varies between drivers.
+        limit_result: :class:`int`
+            Limit how many results are returned,
+            if < 0, no limit is set
         model: :class:`Model`
-            Model to convert results to,
-            if no model present returns raw result
+            Model to return data as, 
+            if no model binded to location, this model becomes the default
+        raw: Any
+            Raw query data instead of using an auto generated one,
+            allows flexibility for some drivers such SQL based drivers
+        no_handle: :class:`bool`
+            Whether to just return the raw fetched data
+        ignore_model: :class:`bool`
+            Whether to return data as dict instead of model
+        **kwds:
+            Additional kwargs may be provided by other drivers which may require any
         """
 
     @abstractmethod
-    def export(self, segment: Segment, include: set, exclude: set) -> None:
-        """ Exports data from segment
+    def export(self, stream: Iterator[Union[dict, Model]], include: set, exclude: set) -> None:
+        """ Exports data from any object supporting the `__next__` method
 
         Parameters
         ----------
-        segment: :class:`Segment`
-            Segment to export
+        stream: Iterator[Union[:class:`dict`, :class:`Model`]]
+            An object which returns values to export
         include: :class:`set`
             set of IDs to include
         exclude: :class:`set`
             set of IDs to exclude
         """
-
-    @abstractmethod
-    def refresh(self) -> None:
-        """ Re-establishes connection to object """
